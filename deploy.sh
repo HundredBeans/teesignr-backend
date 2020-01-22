cad810e3199d80978d976312410265766063d4a8
Mohammad Daffa <daffa@alterra.id>
2020-01-22 14:49:40 +0700

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ git pull
 
 source ~/.profile
 echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
-docker stop tugasDeploy
-docker rm tugasDeploy
+docker stop teesignrBE
+docker rm teesignrBE
 docker rmi daffa99/containerd:BE2
 docker run -d --name teesignrBE -p 5000:5000 daffa99/containerd:BE2
 
